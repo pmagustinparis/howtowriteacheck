d4341a975bcac4db586cf6e361279484ae032bcd
Agustin Paris <parisagustin@gmail.com>
2025-04-28 09:49:37 -0300

    feat(seo): add canonical link to head for all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google-adsense-account" content="ca-pub-7284807519318213" />
+        <link rel="canonical" href="https://www.howwriteacheck.com/" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7284807519318213"
