f96a48e3cdafd13291db6ba60af2126f943af148
Agustin Paris <parisagustin@gmail.com>
2025-04-28 09:35:36 -0300

    feat: add AdSense meta tag for site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-7284807519318213" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7284807519318213"
